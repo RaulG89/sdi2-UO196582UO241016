9b6599667e93c14acf395f0f1dd9d28bece35f68
MarcosRV87 <neverstepalone@gmail.com>
2018-05-01 14:08:44 +0200

    Implemented User Sign-up.

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 //Modules
 var express = require('express');
 var app = express();
+var crypto = require('crypto');
+var mongo = require('mongodb');
 var swig = require('swig');
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 app.use(express.static('public'));
 
+//Variables
+app.set('port',8081);
+app.set('db','mongodb://admin:sdi@ds245228.mlab.com:45228/tiendamusica');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 
 //Routers/controllers
-require("./routes/rusers.js")(app, swig);
-require("./routes/rrequests.js")(app, swig);
+require("./routes/rusers.js")(app, swig, gestorBD);
+require("./routes/rrequests.js")(app, swig, gestorBD);
 
 //Server launcher
 app.set('port', 8081);
--- routes/rusers.js
+++ routes/rusers.js
@@ -1,7 +1,23 @@
-module.exports = function(app , swig) {
+module.exports = function(app , swig, gestorBD) {
     app.get("/signup", function(req, res) {
-        res.send("Registrarse");
+        var respuesta = swig.renderFile('views/signup.html', {});
+        res.send(respuesta);
     });
+    app.post('/usuario', function(req, res) {
+        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        var usuario = {
+            email : req.body.email,
+            password : seguro
+        }
+        gestorBD.insertarUsuario(usuario, function(id) {
+            if (id == null) {
+                res.redirect("/signup?mensaje=Error al registrar usuario")
+            } else {
+                res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+            }
+        });
+    })
 
     app.get("/signin", function(req,res){
         res.send("Logearse");
