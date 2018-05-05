7d6e05ee01dfea63d81fcf60c58f4cd67bb0e4ff
MarcosRV87 <neverstepalone@gmail.com>
2018-05-05 14:36:40 +0200

    Implemented send friend request with validations. Tried to fix search, but not working as intended yet (have to check pagination bug).

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -5,6 +5,26 @@ module.exports = {
 		this.mongo = mongo;
 		this.app = app;
 	},
+    obtenerUsuariosPg : function(criterio, pg, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('usuarios');
+                collection.count(function(err, count) {
+                    collection.find(criterio).skip((pg - 1) * 5).limit(5)
+                        .toArray(function(err, usuarios) {
+                            if (err) {
+                                funcionCallback(null);
+                            } else {
+                                funcionCallback(usuarios, count);
+                            }
+                            db.close();
+                        });
+                });
+            }
+        });
+    },
 	insertarUsuario : function(usuario, funcionCallback) {
 		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
 			if (err) {
@@ -33,6 +53,57 @@ module.exports = {
                         funcionCallback(null);
                     } else {
                         funcionCallback(usuarios);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    addFriendRequest : function(request, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('requests');
+                collection.insert(request, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    getRequests : function(criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('requests');
+                collection.find(criterio).toArray(function(err, requests) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(requests);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    getFriendships : function(criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('friendships');
+                collection.find(criterio).toArray(function(err, friendships) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(friendships);
                     }
                     db.close();
                 });
--- routes/rusers.js
+++ routes/rusers.js
@@ -52,7 +52,95 @@ module.exports = function(app , swig, gestorBD) {
     });
 
     app.get("/user/list", function(req,res){
-        res.send("Logearse");
+        var criterio = {};
+
+        if (req.query.busqueda != null) {
+            criterio = {$and: [{
+                    "email": {$ne: req.session.usuario}
+                }, {
+                    $or: [{
+                        "email": {
+                            $regex: "." + req.query.busqueda + "."
+                        }
+                    }, {
+                        "nombre": {
+                            $regex: "." + req.query.busqueda + "."
+                        }
+                    }]
+                }]
+            };
+        }else{
+            criterio = {"email" : {$ne: req.session.usuario}};
+        }
+
+        var pg = parseInt(req.query.pg); // Es String !!!
+        if (req.query.pg == null) { // Puede no venir el param
+            pg = 1;
+        }
+        gestorBD.obtenerUsuariosPg(criterio, pg, function(usuarios, total) {
+            if (usuarios == null) {
+                res.send("Error al listar ");
+            } else {
+
+                var pgUltima = total / 5;
+                if (total % 5 > 0) { // Sobran decimales
+                    pgUltima = pgUltima + 1;
+                }
+
+                var respuesta = swig.renderFile('views/users.html', {
+                    usuarios : usuarios,
+                    pgActual : pg,
+                    pgUltima : pgUltima
+                });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/friendrequest/:id', function(req, res) {
+        var criterio = { email : req.session.usuario};
+        gestorBD.obtenerUsuarios(criterio, function(usuarios){
+            if(usuarios == null || usuarios.length == 0) {
+                res.redirect("/user/list"
+                    + "?mensaje=Error al enviar petición"
+                    + "&tipoMensaje=alert-danger ");
+            }else {
+                var request = {
+                    requested: gestorBD.mongo.ObjectID(req.params.id),
+                    requesting: usuarios[0]._id
+                }
+                gestorBD.getRequests(request, function(requests) {
+                    if(requests.length != 0){
+                        res.redirect("/user/list"
+                            + "?mensaje=Error al enviar petición, ya existe una petición enviada."
+                            + "&tipoMensaje=alert-danger ");
+                    }else{
+                        var friendship = {
+                            requested: gestorBD.mongo.ObjectID(req.params.id),
+                            requesting: usuarios[0]._id
+                        }
+                        gestorBD.getFriendships(friendship, function(friendships){
+                            if(friendships.length != 0){
+                                res.redirect("/user/list"
+                                    + "?mensaje=Error al enviar petición, ya existe una amistad con este usuario"
+                                    + "&tipoMensaje=alert-danger ");
+                            }else{
+                                gestorBD.addFriendRequest(request, function(idRequest) {
+                                    if (idRequest == null) {
+                                        res.send(respuesta);
+                                    } else {
+                                        res.redirect("/user/list"
+                                        + "?mensaje=Petición de amistad enviada correctamente."
+                                        + "&tipoMensaje=alert-success ");
+                                    }
+                                });
+                            }
+                        })
+                    };
+                });
+
+            }
+        })
     });
 
     app.get("/friends", function(req,res){
