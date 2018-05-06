64369ce0577819882412eaa081a38d381854f34e
Raúl Gómez Pérez <raul.infotek@gmail.com>
2018-05-06 03:08:58 +0200

    Added a comment on the friend list route and restructured the code.

--- routes/rusers.js
+++ routes/rusers.js
@@ -1,44 +1,44 @@
-module.exports = function(app , swig, gestorBD) {
-    app.get("/signup", function(req, res) {
+module.exports = function (app, swig, gestorBD) {
+    app.get("/signup", function (req, res) {
         var respuesta = swig.renderFile('views/signup.html', {});
         res.send(respuesta);
     });
-    app.post('/usuario', function(req, res) {
+    app.post('/usuario', function (req, res) {
         var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         var usuario = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-        var criterio = { email : req.body.email };
-        gestorBD.obtenerUsuarios(criterio, function(usuarios){
-           if(usuarios == null || usuarios.length == 0){
-               gestorBD.insertarUsuario(usuario, function(id) {
-                   if (id == null) {
-                       res.redirect("/signup?mensaje=Error al registrar usuario")
-                   } else {
-                       res.redirect("/signin?mensaje=Nuevo usuario registrado");
-                   }
-               });
-           }else{
-               res.redirect("/signup?mensaje=Ya existe un usuario con este email.")
-           }
+        var criterio = {email: req.body.email};
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                gestorBD.insertarUsuario(usuario, function (id) {
+                    if (id == null) {
+                        res.redirect("/signup?mensaje=Error al registrar usuario")
+                    } else {
+                        res.redirect("/signin?mensaje=Nuevo usuario registrado");
+                    }
+                });
+            } else {
+                res.redirect("/signup?mensaje=Ya existe un usuario con este email.")
+            }
         });
 
     });
 
-    app.get("/signin", function(req, res) {
+    app.get("/signin", function (req, res) {
         var respuesta = swig.renderFile('views/signin.html', {});
         res.send(respuesta);
     });
-    app.post("/signin", function(req, res) {
+    app.post("/signin", function (req, res) {
         var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         var criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
                 res.redirect("/signin"
@@ -51,11 +51,12 @@ module.exports = function(app , swig, gestorBD) {
         });
     });
 
-    app.get("/user/list", function(req,res){
+    app.get("/user/list", function (req, res) {
         var criterio = {};
 
         if (req.query.busqueda != null) {
-            criterio = {$and: [{
+            criterio = {
+                $and: [{
                     "email": {$ne: req.session.usuario}
                 }, {
                     $or: [{
@@ -69,15 +70,15 @@ module.exports = function(app , swig, gestorBD) {
                     }]
                 }]
             };
-        }else{
-            criterio = {"email" : {$ne: req.session.usuario}};
+        } else {
+            criterio = {"email": {$ne: req.session.usuario}};
         }
 
         var pg = parseInt(req.query.pg); // Es String !!!
         if (req.query.pg == null) { // Puede no venir el param
             pg = 1;
         }
-        gestorBD.obtenerUsuariosPg(criterio, pg, function(usuarios, total) {
+        gestorBD.obtenerUsuariosPg(criterio, pg, function (usuarios, total) {
             if (usuarios == null) {
                 res.send("Error al listar ");
             } else {
@@ -88,91 +89,93 @@ module.exports = function(app , swig, gestorBD) {
                 }
 
                 var respuesta = swig.renderFile('views/users.html', {
-                    usuarios : usuarios,
-                    pgActual : pg,
-                    pgUltima : pgUltima
+                    usuarios: usuarios,
+                    pgActual: pg,
+                    pgUltima: pgUltima
                 });
                 res.send(respuesta);
             }
         });
     });
 
-    app.get('/friendrequest/:id', function(req, res) {
-        var criterio = { email : req.session.usuario};
-        gestorBD.obtenerUsuarios(criterio, function(usuarios){
-            if(usuarios == null || usuarios.length == 0) {
+    app.get('/friendrequest/:id', function (req, res) {
+        var criterio = {email: req.session.usuario};
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
                 res.redirect("/user/list"
                     + "?mensaje=Error al enviar petición"
                     + "&tipoMensaje=alert-danger ");
-            }else {
+            } else {
                 var request = {
                     requested: gestorBD.mongo.ObjectID(req.params.id),
                     requesting: gestorBD.mongo.ObjectID(usuarios[0]._id)
                 }
-                gestorBD.obtenerPeticiones(request, function(requests) {
-                    if(requests.length != 0){
+                gestorBD.obtenerPeticiones(request, function (requests) {
+                    if (requests.length != 0) {
                         res.redirect("/user/list"
                             + "?mensaje=Error al enviar petición, ya existe una petición enviada."
                             + "&tipoMensaje=alert-danger ");
-                    }else{
+                    } else {
                         var friendship = {
                             requested: gestorBD.mongo.ObjectID(req.params.id),
                             requesting: usuarios[0]._id
                         }
-                        gestorBD.obtenerAmistades(friendship, function(friendships){
-                            if(friendships.length != 0){
+                        gestorBD.obtenerAmistades(friendship, function (friendships) {
+                            if (friendships.length != 0) {
                                 res.redirect("/user/list"
                                     + "?mensaje=Error al enviar petición, ya existe una amistad con este usuario"
                                     + "&tipoMensaje=alert-danger ");
-                            }else{
-                                gestorBD.addFriendRequest(request, function(idRequest) {
+                            } else {
+                                gestorBD.addFriendRequest(request, function (idRequest) {
                                     if (idRequest == null) {
                                         res.send(respuesta);
                                     } else {
                                         res.redirect("/user/list"
-                                        + "?mensaje=Petición de amistad enviada correctamente."
-                                        + "&tipoMensaje=alert-success ");
+                                            + "?mensaje=Petición de amistad enviada correctamente."
+                                            + "&tipoMensaje=alert-success ");
                                     }
                                 });
                             }
                         })
-                    };
+                    }
+                    ;
                 });
 
             }
         })
     });
 
-
-    app.get("/friends", function(req,res){
+    /*
+    * Route that list all friends of the user in session.
+    *
+    */
+    app.get("/friends", function (req, res) {
         var pg = parseInt(req.query.pg);
         if (req.query.pg == null) {
             pg = 1;
         }
         var criterion = {
-            email : req.session.usuario
+            email: req.session.usuario
         };
-        gestorBD.obtenerUsuarios(criterion, pg, function(users){
-            if (users == null){
-                res.send("Error al listar amigos.");
-            } else {
+        gestorBD.obtenerUsuarios(criterion, pg, function (users) {
+            if (users != null) {
                 var friendCriterion = {
-                    user : gestorBD.mongo.ObjectID(users[0]._id)
+                    user: gestorBD.mongo.ObjectID(users[0]._id)
                 };
 
-                gestorBD.getFriends(friendCriterion, function(friendship){
+                gestorBD.getFriends(friendCriterion, function (friendship) {
                     var localUser;
                     var localUserId = [];
 
-                    for(x in friendship){
+                    for (f in friendship) {
                         localUserId.push(
-                            gestorBD.mongo.ObjectID(friendship[x].localUser)
+                            gestorBD.mongo.ObjectID(friendship[f].localUser)
                         )
                     }
 
                     localUser = {
-                        _id : {
-                            $in : localUserId
+                        _id: {
+                            $in: localUserId
                         }
                     };
 
@@ -181,25 +184,27 @@ module.exports = function(app , swig, gestorBD) {
                         pg = 1;
                     }
 
-                    gestorBD.obtenerUsuariosPg(localUser, pg, function(
-                       friends, total) {
+                    gestorBD.obtenerUsuariosPg(localUser, pg, function (
+                        friends, total) {
                         var lastPg = total / 5;
 
                         if (total % 5 > 0) {
-                           lastPg = lastPg + 1;
+                            lastPg = lastPg + 1;
                         }
 
                         var response = swig.renderFile(
                             'views/users.html', {
-                                users : friends,
-                                currentPg : pg,
-                                lastPg : lastPg,
+                                users: friends,
+                                currentPg: pg,
+                                lastPg: lastPg,
                                 userSession: req.session.user
                             });
 
                         res.send(response);
                     });
                 });
+            } else {
+                res.send("Error al listar amigos.");
             }
         });
     });
