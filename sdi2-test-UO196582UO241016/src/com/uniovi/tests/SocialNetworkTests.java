e77d90e79b0d0ca87e716ca066c4d50ef2ebac11
MarcosRV87 <neverstepalone@gmail.com>
2018-05-10 18:16:23 +0200

    Selenium Test currently working properly in one execution. Need to implement a method to erase the database.

--- sdi2-test-UO196582UO241016/src/com/uniovi/tests/SocialNetworkTests.java
+++ sdi2-test-UO196582UO241016/src/com/uniovi/tests/SocialNetworkTests.java
@@ -71,33 +71,33 @@ public class SocialNetworkTests {
 	}
 
 	// 1.1 [RegVal] Registro de Usuario con datos válidos
-
-	public void RegVal() {
+	@Test
+	public void PR01_RegVal() {
 		// Vamos al formulario de registro
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonSignup')]");
 		elementos.get(0).click();
 		// Rellenamos el formulario.
-		PO_RegisterView.fillForm(driver, "Pocito", "pocito32@gmail.com", "pocito123", "pocito123");
+		PO_RegisterView.fillForm(driver, "RegVal", "regval@gmail.com", "regval123", "regval123");
 		// Comprobamos que se ha registrado con éxito
 		SeleniumUtils.textoPresentePagina(driver, "Nuevo usuario registrado");
 	}
 
 	// 1.2 [RegInval] Registro de Usuario con datos inválidos (repetición de
 	// contraseña invalida).
-
-	public void RegInval() {
+	@Test
+	public void PR02_RegInval() {
 		// Vamos al formulario de registro
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonSignup')]");
 		elementos.get(0).click();
 		// Rellenamos el formulario.
-		PO_RegisterView.fillForm(driver, "Pocito", "pocito32@gmail.com", "pocito1234", "pocito123");
+		PO_RegisterView.fillForm(driver, "RegInval", "reginval@gmail.com", "unpassword", "otropassword");
 		// Comprobamos que se ha registrado con éxito
 		SeleniumUtils.textoPresentePagina(driver, "Las contraseñas han de coincidir");
 	}
 
 	// 2.1 [InVal] Inicio de sesión con datos válidos.
-
-	public void InVal() {
+	@Test
+	public void PR03_InVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -109,8 +109,8 @@ public class SocialNetworkTests {
 
 	// 2.2 [InInVal] Inicio de sesión con datos inválidos (usuario no existente en
 	// la aplicación)..
-
-	public void InInVal() {
+	@Test
+	public void PR04_InInVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -123,8 +123,8 @@ public class SocialNetworkTests {
 	}
 
 	// 3.1 [LisUsrVal] Acceso al listado de usuarios desde un usuario en sesión
-
-	public void LisUsrVal() {
+	@Test
+	public void PR05_LisUsrVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -141,15 +141,15 @@ public class SocialNetworkTests {
 	// vistas
 	// privadas.
 	@Test
-	public void LisUsrInVal() {
+	public void PR06_LisUsrInVal() {
 		driver.navigate().to("http://localhost:8081/user/list");
 		SeleniumUtils.textoPresentePagina(driver, "Intento de acceso a una zona privada sin autorizacion");
 	}
 
 	// 4.1 [BusUsrVal] Realizar una búsqueda valida en el listado de usuarios desde
 	// un usuario en sesión.
-
-	public void BusUsrVal() {
+	@Test
+	public void PR07_BusUsrVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -167,14 +167,14 @@ public class SocialNetworkTests {
 	// un usuario no
 	// identificado. Debe producirse un acceso no permitido a vistas privadas.
 	@Test
-	public void BusUsrInVal() {
+	public void PR08_BusUsrInVal() {
 		driver.navigate().to("http://localhost:8081/user/list?busqueda=quemasda");
 		SeleniumUtils.textoPresentePagina(driver, "Intento de acceso a una zona privada sin autorizacion");
 	}
 
 	// 5.1 [InvVal] Enviar una invitación de amistad a un usuario de forma valida.
-
-	public void InvVal() {
+	@Test
+	public void PR09_InvVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -182,7 +182,7 @@ public class SocialNetworkTests {
 		PO_LoginView.fillForm(driver, "yeyas@gmail.com", "yeyas123");
 		// COmprobamos que entramos en la pagina privada
 		SeleniumUtils.textoPresentePagina(driver, "Usuarios");
-		By enlace = By.xpath("//td[contains(text(), 'Prueba2')]/following-sibling::*[2]");
+		By enlace = By.xpath("//td[contains(text(), 'Prueba3')]/following-sibling::*[2]");
 		SeleniumUtils.esperarSegundos(driver, 1);
 		driver.findElement(enlace).click();
 		// Comprobación
@@ -194,8 +194,8 @@ public class SocialNetworkTests {
 	// previamente. No debería dejarnos enviar la invitación, se podría ocultar el
 	// botón de enviar invitación o
 	// notificar que ya había sido enviada previamente.
-
-	public void InvInVal() {
+	@Test
+	public void PR10_InvInVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -203,7 +203,7 @@ public class SocialNetworkTests {
 		PO_LoginView.fillForm(driver, "yeyas@gmail.com", "yeyas123");
 		// COmprobamos que entramos en la pagina privada
 		SeleniumUtils.textoPresentePagina(driver, "Usuarios");
-		By enlace = By.xpath("//td[contains(text(), 'Prueba2')]/following-sibling::*[2]");
+		By enlace = By.xpath("//td[contains(text(), 'Prueba3')]/following-sibling::*[2]");
 		SeleniumUtils.esperarSegundos(driver, 1);
 		driver.findElement(enlace).click();
 		// Comprobación
@@ -213,32 +213,30 @@ public class SocialNetworkTests {
 	// 6.1 [LisInvVal] Listar las invitaciones recibidas por un usuario, realizar la
 	// comprobación con una lista
 	// que al menos tenga una invitación recibida.
-
-	public void LisInvVal() {
+	@Test
+	public void PR11_LisInvVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
 		// Rellenamos el formulario
-		PO_LoginView.fillForm(driver, "yeyas@gmail.com", "yeyas123");
+		PO_LoginView.fillForm(driver, "prueba3@prueba3.com", "prueba3");
 		// COmprobamos que entramos en la pagina privada
 		SeleniumUtils.textoPresentePagina(driver, "Usuarios");
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href,'/requests')]");
 		elementos.get(0).click();
-		// Comprobamos que aparecen dos botones de aceptar, por lo tanto,
-		// existen 2
-		// peticiones de amistad pendientes.
+		// Comprobamos
 		elementos = PO_View.checkElement(driver, "free", "//td/following-sibling::*[1]");
 		assertTrue(elementos.size() == 1);
 	}
 
 	// 7.1 [AcepInvVal] Aceptar una invitación recibida
-
-	public void AcepInvVal() {
+	@Test
+	public void PR12_AcepInvVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
 		// Rellenamos el formulario
-		PO_LoginView.fillForm(driver, "yeyas@gmail.com", "yeyas123");
+		PO_LoginView.fillForm(driver, "prueba3@prueba3.com", "prueba3");
 		// COmprobamos que entramos en la pagina privada
 		SeleniumUtils.textoPresentePagina(driver, "Usuarios");
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href,'/requests')]");
@@ -253,8 +251,8 @@ public class SocialNetworkTests {
 
 	// 8.1 [ListAmiVal] Listar los amigos de un usuario, realizar la comprobación con una lista que al menos
 	// tenga un amigo.
-	
-	public void ListAmiVal() {
+	@Test	
+	public void PR13_ListAmiVal() {
 		// Vamos al formulario de logueo.
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonLogin')]");
 		elementos.get(0).click();
@@ -264,7 +262,7 @@ public class SocialNetworkTests {
 		SeleniumUtils.textoPresentePagina(driver, "Usuarios");
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href,'/friends')]");
 		elementos.get(0).click();
-		elementos = PO_View.checkElement(driver, "free", "//td[contains(text(), 'Prueba1')]");
+		elementos = PO_View.checkElement(driver, "free", "//td[contains(text(), 'Prueba3')]");
 		assertTrue(elementos.size() == 1);
 	}
 
